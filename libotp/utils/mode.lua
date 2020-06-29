89015a6be9359461323144d1172afa2cb0416975
signalhunter <boom@flyingpackets.net>
2020-06-29 14:19:14 -0400

    libotp: Improved hexamode input validation

--- libotp/utils/mode.lua
+++ libotp/utils/mode.lua
@@ -5,20 +5,29 @@
 local base = require("libotp.utils.base")
 local mode = {}
 
+local function checkAlphabet(str,pat)
+  local ctr = 0
+  for i in str:gmatch(pat) do
+    ctr = ctr+1
+  end
+  if not (ctr == #str) then return false,ctr end
+  return true,ctr
+end
+
 -- Decodes hexadecimal and returns a boolean table
 function mode.getMode(s,l)
+  -- Input validation
+  if not (type(s) == "string") then return false end
+  if not (#s == l) then return false end
+  if not (#s % 2 == 0) then return false end
+  if not checkAlphabet(s:lower(),"[0-9a-f]") then return false end
+
+  -- Conversion
   local tbl = {}
-  if (#s == l) and (#s % 2 == 0) then
-    if not s:lower():gmatch("[0-9a-f]") then
-      return false
-    end
-    for b in base.to_bit(base.from_hex(s)):gmatch("%d") do
-      if b == "1" then table.insert(tbl,true) else table.insert(tbl,false) end
-    end
-    return tbl
-  else
-    return false
+  for b in base.to_bit(base.from_hex(s)):gmatch("%d") do
+    if b == "1" then table.insert(tbl,true) else table.insert(tbl,false) end
   end
+  return tbl
 end
 
 return mode
