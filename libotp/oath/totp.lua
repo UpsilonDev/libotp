8dd3ed56348cce45177bfe21bbef96e502351bf0
signalhunter <boom@flyingpackets.net>
2020-09-08 00:55:12 -0400

    Fix OTP

--- libotp/oath/totp.lua
+++ libotp/oath/totp.lua
@@ -8,13 +8,12 @@ local oath = require("libotp.oath.common")
 
 local totp = {}
 
--- note: this doesn't work, yet
 function totp.generate(s,o,l)
   local c = math.floor((os.epoch("utc")/1000)/(o or 30))
   local otp = oath.truncate(
     sha1.hmac(
       oath.counter(c),
-      base.to_base32(s)
+      base.from_base32(s)
     )
   )
   return oath.process(otp,(l or 6))
