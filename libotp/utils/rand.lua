ecf20a0666435b4f5bbb6f8b50c9be26f35c3d4f
signalhunter <boom@flyingpackets.net>
2020-09-08 19:03:52 -0400

    Implement OATH seed generator

--- libotp/utils/rand.lua
+++ libotp/utils/rand.lua
@@ -2,18 +2,23 @@
 -- Part of the libotp library for ComputerCraft
 -- https://github.com/UpsilonDev/libotp
 
+local base = require("libotp.utils.base")
 local csprng = require("libotp.crypto.csprng")
 
 local rand = {}
 
-function rand.getRandomString(l)
+local function randStr(l,r)
   -- Who needs a iterator buffer when you can use rep and gsub
   csprng.seed_from_mt(os.epoch("utc"))
   csprng.generate_isaac()
   return (tostring(l):rep(l):gsub(".",function(i)
-    local r = {{48,57},{65,90},{97,122}} -- Alphanumeric range
-    return string.char(csprng.randomRange(unpack(r[csprng.randomRange(1,3)])))
+    return string.char(csprng.randomRange(unpack(r[csprng.randomRange(1,#r)])))
   end))
+end
+
+function rand.getRandomString(l)
+  local r = {{48,57},{65,90},{97,122}} -- Alphanumeric range
+  return randStr(l,r)
 end
 
 -- Convenience wrapper for randomRange
@@ -21,6 +26,12 @@ function rand.randomRange(min,max)
   csprng.seed_from_mt(os.epoch("utc"))
   csprng.generate_isaac()
   return csprng.randomRange(min,max)
+end
+
+-- Generate random OATH TOTP/HOTP seed
+function rand.getRandomSeed()
+  local r = {{33,47},{48,57},{58,64},{65,90},{91,96},{97,122}}
+  return randStr(10,r)
 end
 
 return rand
